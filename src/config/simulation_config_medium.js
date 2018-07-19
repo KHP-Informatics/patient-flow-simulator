92056e99b52fd68daf760a14e7d25d056d1d649a
Daniel Bean <daniel.bean@kcl.ac.uk>
2018-07-19 23:21:58 +0100

    rebalance and explain costs

--- src/config/simulation_config_medium.js
+++ src/config/simulation_config_medium.js
@@ -3,7 +3,7 @@
 var simulation_config = {
 	steps: 24, //was 672
 	emergency_wait_target: 4,
-	staff_cost: 50,
-	resource_cost: 10,
-	bed_cost: 100
+	staff_cost: 150,
+	resource_cost: 150,
+	bed_cost: 400
 }
